15286d93ebff5a75ee5e2b755259736e20c615fe
Nathaly <natharb09@gmail.com>
2020-03-04 21:49:27 -0500

    feat: matching url and a path

--- router.js
+++ router.js
@@ -1,12 +1,25 @@
 class Router {
   constructor(routes) {
     this.routes = routes;
-    this.loadInitialRoutes();
+    this._loadInitialRoutes();
   }
-}
 
-loadInitialRoutes() {
-  const pathNameSplit = window.location.pathname.split('/');
-  const pathSegs = pathNameSplit.length > 1 ? pathNameSplit(1) : '';
-  this.loadRoute(...pathSegs);
+  _matchUrlToRoute(urlSegs) {
+    const matchedRoute = this.routes.find(route => {
+      const routePathSegs = route.path.split('/').slice(1);
+      if (routePathSegs.length !== urlSegs.length) {
+        return false;
+      }
+
+      return routePathSegs.every((routePathSeg, i) => routePathSeg === urlSegs[i]);
+    });
+
+    return matchedRoute;
+  }
+
+  _loadInitialRoutes() {
+    const pathNameSplit = window.location.pathname.split('/');
+    const pathSegs = pathNameSplit.length > 1 ? pathNameSplit(1) : '';
+    this.loadRoute(...pathSegs);
+  }
 }
